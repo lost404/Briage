64dbec3ce3fd6b01d32f8c551f4d215dedb77ed0
lost404 <lost404@163.com>
2012-11-11 19:46:48 +0800

    about.txt

---
+++ about.txt
@@ -0,0 +1,14 @@
+什么是Briage?
+	Briage是一款开源的WEB开发框架，它主张更加快速的在小团队中开发大型应用。
+
+成员：
+	lost404(lost404@163.com)
+	lxy
+	vox
+	jack-yyf
+
+案例：
+	草旅网(http://www.caovel.com)
+
+加入我们：
+	lost404@163.com
--- test/cdqz.net.php
+++
@@ -1,6 +0,0 @@
-http://www.cdqz.net/upfile/images/1207080910.php
-admin/fckeditor/fckeditor.js.php
-
-http://www.cdqz.net/upfile/images/111114162720172751b6a7jfryjn7b1.php
-
-d:\cdqz.city668.com\upfile\images\120411114838984376hn42w94wyhkz.exe
